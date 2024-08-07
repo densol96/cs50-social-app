df56cbf8a0c3bd879dbbbf2a54d52dbce4df733e
Deniss Solovjovs <deniss11sol@gmail.com>
2024-08-07 23:02:40 +0300

    NEW: Project set up with React(Vite) and Spring Boot.

---
+++ back-end/src/main/java/cs/densol/back_end/BackEndApplication.java
@@ -0,0 +1,14 @@
+package cs.densol.back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndApplication.class, args);
+		System.out.println("Hello world!");
+	}
+
+}
